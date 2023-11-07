807fa839c8719848736a25861280c8a433016a81
纾浚 <junical@email.cn>
2023-11-08 04:28:00 +0800

    加了点注释

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// 引入依赖库
 use inquire::{
     validator::Validation,
     Text,
@@ -6,13 +7,16 @@ use fancy_regex::Regex;
 use reqwest::blocking as req;
 use reqwest::redirect::Policy;
 
+// 常量部分，主要用于正则表达式匹配和B站API
 const REG_BVID: &str = r"BV\w{10}";
 const REG_AVID: &str = r"av\d{1,9}";
 // const REG_URL: &str = r"(.*)bilibili.com/video/(BV\w{10}|av\d{1,9})";
 const REG_URL:&str = r"(.*)bilibili.com/video/(BV\w{10}|av\d{1,9})(?=/|\?|$)";
 const REG_SHORT_URL: &str = r"(http(s|)://|^)b23.tv/(\w+)";
 
+// 主函数，主要处理用户输入和程序整体流程
 fn main() {
+    // inquire预验证规则，只按照正则表达式进行匹配判断
     let validator = |input: &str| {
         if Regex::new(REG_AVID).unwrap().is_match(input).unwrap()||
             Regex::new(REG_BVID).unwrap().is_match(input).unwrap() ||
@@ -23,6 +27,8 @@ fn main() {
             Ok(Validation::Invalid("请输入正确的视频链接或BV/av号".into()))
         }
     };
+
+    // 简单提取常规长url中的av/bv号，用于inquire回显展示，防止长链接换行不美观（av/bv号输入和短链接输入不管）
     let format_to_id = &|input: &_| {
         if Regex::new(REG_URL).unwrap().is_match(input).unwrap() {
             match Regex::new(REG_BVID).unwrap().captures(input).unwrap() {
@@ -34,12 +40,17 @@ fn main() {
             input.to_string()
         }
     };
+
+    // 构造inquire请求对象
     let video_inquirer = Text::new("请输入要下载的视频链接或BV/av号")
         .with_help_message("B站视频链接、b23.tv短连接、BV号或av号均可")
         .with_validator(validator)
         .with_formatter(format_to_id);
+
     let mut input_invalid = true;
     let mut video_id = String::new();
+
+    // 询问+处理逻辑，当处理出错时（短链接404、长链接格式有误等正则查不出来等错误）循环提示用户重新输入
     while input_invalid {
         let res = video_inquirer.clone().prompt().unwrap().to_string();
         match parse_video_id(&res) {
@@ -50,6 +61,7 @@ fn main() {
     println!("{}", video_id);
 }
 
+// 将用户输入的视频url、短链接、av/bv号等统一处理成av/bv号，方便后续请求
 fn parse_video_id (input:&str) -> Result<String, &str> {
     let reg_bvid = Regex::new(REG_BVID).unwrap();
     let reg_avid = Regex::new(REG_AVID).unwrap();
@@ -86,6 +98,7 @@ fn parse_video_id (input:&str) -> Result<String, &str> {
     }
 }
 
+// 解析b23.tv短链接
 fn parse_short_url(short_url:&str) -> Option<String> {
     let client = req::Client::builder()
         .redirect(Policy::none())
