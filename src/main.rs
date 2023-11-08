aa1945d5158e468090b80d3df6b1a9192d475b78
纾浚 <junical@email.cn>
2023-11-08 23:42:51 +0800

    视频信息获取完工

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@ use inquire::{
     Text,
 };
 use fancy_regex::Regex;
-use reqwest::blocking as req;
-use reqwest::redirect::Policy;
+use reqwest::{
+    blocking as req,
+    redirect::Policy,
+    header,
+};
+use serde::Deserialize;
+use colored::*;
 
 // 常量部分，主要用于正则表达式匹配和B站API
 const REG_BVID: &str = r"BV\w{10}";
@@ -13,14 +18,58 @@ const REG_AVID: &str = r"av\d{1,9}";
 // const REG_URL: &str = r"(.*)bilibili.com/video/(BV\w{10}|av\d{1,9})";
 const REG_URL: &str = r"(.*)bilibili.com/video/(BV\w{10}|av\d{1,9})(?=/|\?|$)";
 const REG_SHORT_URL: &str = r"(http(s|)://|^)b23.tv/(\w+)";
-
 const API_VIDEO_INFO: &str = "https://api.bilibili.com/x/web-interface/view";
 const API_STREAM_URL: &str = "https://api.bilibili.com/x/player/wbi/playurl";
 const API_USER_INFO: &str = "https://api.bilibili.com/x/web-interface/nav";
+const HTTP_REFERER: &str = "www.bilibili.com";
 
-enum VideoId {
+enum VideoIdValue {
     Avid(u32),
-    Bvid(String)
+    Bvid(String),
+}
+
+struct VideoId {
+    value: VideoIdValue,
+}
+
+impl VideoId {
+    fn get_key(&self) -> &str {
+        match self.value {
+            VideoIdValue::Avid(_) => "aid",
+            VideoIdValue::Bvid(_) => "bvid"
+        }
+    }
+    fn to_string(&self) -> String {
+        match &self.value {
+            VideoIdValue::Avid(t) => t.to_string(),
+            VideoIdValue::Bvid(t) => t.clone(),
+        }
+    }
+    fn new(av_or_bvid: &str) -> Result<Self, String> {
+        match Regex::new(REG_AVID).unwrap().captures(av_or_bvid).unwrap() {
+            Some(t) => match t[0][2..].parse::<u32>() {
+                Ok(t) => Ok(Self { value: VideoIdValue::Avid(t) }),
+                Err(t) => Err(t.to_string())
+            },
+            None => match Regex::new(REG_BVID).unwrap().captures(av_or_bvid).unwrap() {
+                Some(t) => Ok(Self { value: VideoIdValue::Bvid(t[0].to_string()) }),
+                None => Err("在输入的字符串中未找到有效的av/bvid".into())
+            }
+        }
+    }
+}
+
+struct PageInfo {
+    cid: u32,
+    p: u32,
+    title: String,
+}
+
+struct VideoInfo {
+    bvid: String,
+    title: String,
+    uploader: String,
+    pages: Vec<PageInfo>,
 }
 
 // 主函数，主要处理用户输入和程序整体流程
@@ -57,9 +106,21 @@ fn main() {
         .with_formatter(format_to_id);
 
     let mut input_invalid = true;
-    let mut video_id = VideoId::Avid(0);
+    let mut video_id = VideoId::new("av0").unwrap();
+    let mut video_info = VideoInfo {
+        bvid: "".into(),
+        title: "".into(),
+        uploader: "".into(),
+        pages: Vec::new(),
+    };
 
     let cookie = Text::new("请输入Cookie SESSDATA =").prompt().unwrap();
+    let mut headers = header::HeaderMap::new();
+    headers.insert(header::REFERER, header::HeaderValue::from_static(HTTP_REFERER));
+    headers.insert(header::COOKIE, header::HeaderValue::from_str(&*format!("SESSDATA={}", &cookie)).unwrap());
+    let client = req::Client::builder()
+        .default_headers(headers)
+        .build().unwrap();
 
     // 询问+处理逻辑，当处理出错时（短链接404、长链接格式有误等正则查不出来等错误）循环提示用户重新输入
     while input_invalid {
@@ -69,51 +130,48 @@ fn main() {
                 video_id = t;
                 input_invalid = false;
             }
-            Err(e) => println!("{}", e)
+            Err(e) => println!("{}", e.bold().red())
+        };
+        match get_video_info(&video_id, &client) {
+            Ok(t) => video_info = t,
+            Err(e) => {
+                println!("{}", e.bold().red());
+                input_invalid = true;
+            }
         }
     }
-    match video_id {
-        VideoId::Avid(t) => println!("{}",t),
-        VideoId::Bvid(t) => println!("{}",t)
-    }
+
+    println!("1")
 }
 
 // 将用户输入的视频url、短链接、av/bv号等统一处理成av/bv号，方便后续请求
-fn parse_video_id(input: &str) -> Result<VideoId, &str> {
+fn parse_video_id(input: &str) -> Result<VideoId, String> {
     let reg_bvid = Regex::new(REG_BVID).unwrap();
     let reg_avid = Regex::new(REG_AVID).unwrap();
     let reg_url = Regex::new(REG_URL).unwrap();
     let reg_short_url = Regex::new(REG_SHORT_URL).unwrap();
-    let url_to_id = |a: &str| -> Result<VideoId, &str> {
+    let url_to_id = |a: &str| -> Result<VideoId, String> {
         let processed_url = match reg_url.captures(a).unwrap() {
             Some(t) => t[0].to_string(),
-            None => return Err("解析视频Url出错")
+            None => return Err("解析视频Url出错".into())
         };
-        match reg_bvid.captures(&processed_url).unwrap() {
-            Some(caps) => Ok(VideoId::Bvid(caps[0].to_string())),
-            None => match reg_avid.captures(input).unwrap() {
-                Some(caps) => Ok(VideoId::Avid(caps[0].to_string()[2..].parse::<u32>().unwrap())),
-                None => Err("解析视频Url出错")
-            }
-        }
+        VideoId::new(&processed_url)
     };
     if reg_url.is_match(input).unwrap() {
         url_to_id(input)
-    } else if reg_bvid.is_match(input).unwrap() {
-        Ok(VideoId::Bvid(input.to_string()))}
-    else if reg_avid.is_match(input).unwrap() {
-        Ok(VideoId::Avid(input[2..].parse::<u32>().unwrap()))
+    } else if reg_bvid.is_match(input).unwrap() || reg_avid.is_match(input).unwrap() {
+        VideoId::new(input)
     } else if reg_short_url.is_match(input).unwrap() {
         let processed_short_url = match reg_short_url.captures(input).unwrap() {
             Some(t) => t[0].to_string(),
-            None => return Err("解析短Url出错")
+            None => return Err("解析短Url出错".into())
         };
         match parse_short_url(&processed_short_url) {
             Some(t) => url_to_id(&t),
-            None => Err("该b23.tv短链接无效")
+            None => Err("该b23.tv短链接无效".into())
         }
     } else {
-        Err("视频链接无效")
+        Err("视频链接无效".into())
     }
 }
 
@@ -135,4 +193,56 @@ fn parse_short_url(short_url: &str) -> Option<String> {
     } else {
         None
     }
+}
+
+// 获取视频信息，也用于预检视频是否有效
+fn get_video_info(video_id: &VideoId, client: &req::Client) -> Result<VideoInfo, String> {
+    #[derive(Deserialize)]
+    struct RawOwner {
+        name: String,
+    }
+    #[derive(Deserialize)]
+    struct RawPage {
+        cid: u32,
+        page: u32,
+        part: String,
+    }
+    #[derive(Deserialize)]
+    struct RawInfo {
+        bvid: String,
+        title: String,
+        owner: RawOwner,
+        pages: Vec<RawPage>,
+    }
+    #[derive(Deserialize)]
+    struct RawResponse {
+        code: u32,
+        data: RawInfo,
+    }
+    let res = client.get(API_VIDEO_INFO)
+        .query(&[(video_id.get_key(), video_id.to_string())])
+        .send();
+    let res = match res {
+        Ok(t) => t,
+        Err(_) => return Err("网络错误".into())
+    };
+    let res: RawResponse = match res.json() {
+        Ok(t) => t,
+        Err(_) => return Err("响应异常，视频可能不存在".into())
+    };
+    if res.code != 0 { return Err(format!("视频状态异常：{}", res.code)); }
+    let mut pages: Vec<PageInfo> = Vec::new();
+    for i in res.data.pages.iter() {
+        pages.push(PageInfo {
+            title: String::from(&i.part),
+            cid: i.cid,
+            p: i.page,
+        })
+    }
+    Ok(VideoInfo {
+        bvid: res.data.bvid,
+        title: res.data.title,
+        uploader: res.data.owner.name,
+        pages,
+    })
 }
